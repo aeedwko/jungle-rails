65ecbbab6b3dc8138e89fd1014791911ad36bf2e
Edwin Ko <edwin.mk.ko@gmail.com>
2024-04-25 21:47:34 -0700

    implement check to see if existing user email is in database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,18 @@ class UsersController < ApplicationController
   end 
 
   def create 
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
+    existing_user = User.find_by_email(params[:user][:email])
+    if existing_user
+      puts "User exists already"
       redirect_to '/signup'
+    else 
+      user = User.new(user_params)
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
     end
   end
 
